12913ff65d7c431e1a8b37ec4c8e87a5bb219e88
zaineb-h <zaineb4281@gmail.com>
2024-11-01 10:22:28 -0400

    Initial commit

---
+++ R/multiplier.R
@@ -0,0 +1,18 @@
+#' A function for multiplying two numbers
+#'
+#' A baby function to illustrate how to document stuff in R
+#' It is soley a function to multiply two numbers
+
+#' @name multiplier
+#' @param a A real number
+#' @param b a real number
+#' @return the product of \code{a} and \code{b}
+#' @export
+#' @examples
+#' multiplier(2, 5)
+#' multiplier(4, 5)
+
+
+multiplier <- function(a, b){
+  a*b
+}
---
+++ data-raw/DATASET.R
@@ -0,0 +1,6 @@
+## code to prepare `DATASET` dataset goes here
+
+dummy_vec <- c(1, 2, 3, 4)
+
+usethis::use_data(dummy_vec, overwrite = TRUE)
+
